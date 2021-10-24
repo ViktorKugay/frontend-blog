e4bdbd2d065ce13d50c1fce333d13f01d9f5b40c
v.kugay <v.kugay@tinkoff.ru>
2021-10-24 17:07:13 +0300

    refactoring: refactor all project to atomic design add atomic design, add api directory, update names of components

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  target: 'experimental-serverless-trace',
+  // target: 'experimental-serverless-trace',
   env: {
     APP_FIRESTORE_CREDENTIALS: process.env.APP_FIRESTORE_CREDENTIALS,
   },
--- src/frontend/hooks/posts/usePostsStore.ts
+++ src/frontend/hooks/posts/usePostsStore.ts
@@ -2,7 +2,7 @@ import {postsStore} from '../../store';
 import {Post} from '../../store/posts/Posts';
 
 export function usePostBySlug(slug: string): Post | undefined {
-  if (slug) return undefined;
+  if (!slug) return undefined;
 
   return postsStore.findOneBySlug(slug);
 }
